72005f97c48fcc143f381f0f5b531c6955efe0d4
Mark <ziplokk.mark.sanders@gmail.com>
2017-04-11 11:37:23 -0500

    Updated setup.py version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.0.3'
+VERSION = '0.0.4'
 
 REQUIREMENTS = [
     'lxml',
@@ -20,5 +20,6 @@ setup(
     author_email='sdscdeveloper@gmail.com',
     install_requires=REQUIREMENTS,
     description='Wrapper modules to request and parse responses for python-amazon-mws.',
-    include_package_data=True
+    include_package_data=True,
+    version=VERSION
 )
